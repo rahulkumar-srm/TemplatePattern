cbb7fdc521d09b76c78b95e68b8c981d9eafe2c7
Rahul Kumar <rahul.kumar14@stryker.com>
2021-08-10 20:21:11 +0530

    Add project files.

---
+++ TemplatePattern/CaffeineBeverage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePattern
+{
+    public abstract class CaffeineBeverage
+    {
+        public void PrepareReceipe()
+        {
+            BoilWater();
+            Brew();
+            PourInCup();
+            AddCondiments();
+        }
+
+        public abstract void Brew();
+        public abstract void AddCondiments();
+
+        void BoilWater()
+        {
+            Console.WriteLine("Boiling Water");
+        }
+
+        void PourInCup()
+        {
+            Console.WriteLine("Pouring into cup");
+        }
+    }
+}
---
+++ TemplatePattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Making Tea...");
+            CaffeineBeverage caffeineBeverage = new Tea();
+            caffeineBeverage.PrepareReceipe();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Making Coffee...");
+            caffeineBeverage = new Coffee();
+            caffeineBeverage.PrepareReceipe();
+
+            Console.ReadKey();
+        }
+    }
+}
